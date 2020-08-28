7194d028dce971a601e27932a3ca9fe95851e8f0
User <nexx01@mail.ru>
2020-08-28 17:15:42 +0300

    ,

--- spring_mvc/src/main/java/web/config/WebConfig.java
+++ spring_mvc/src/main/java/web/config/WebConfig.java
@@ -1,15 +1,23 @@
 package web.config;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -22,6 +30,11 @@ public class WebConfig implements WebMvcConfigurer {
         this.applicationContext = applicationContext;
     }
 
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/1").setViewName("cars");
+    }
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
@@ -41,15 +54,88 @@ public class WebConfig implements WebMvcConfigurer {
         return templateEngine;
     }
 
+/*    @Bean(name = "messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+        messageResource.setBasenames("classpath:1/bundle");
+        //System.out.println( messageSource.getClass().getResource("resources.bundle");
+        messageResource.setDefaultEncoding("UTF-8");
+        return messageResource;
+    }*/
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+
         registry.viewResolver(resolver);
-        //ThymeleafViewResolver viewResolver;
+        ThymeleafViewResolver viewResolver;
+
         resolver.setCharacterEncoding("UTF-8");
         resolver.setContentType("text/html; charset=UTF-8");
-
     }
+
+    @Bean
+    public LocaleResolver sessionLocaleResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        // This interceptor would intercept every single request and set the
+        // locale value from the param name provided on the resolver.
+        //Useful only if the locale value is available through a request parameter.
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("bundle");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+
+
+
+
+
+/*    @Bean
+    public LocaleResolver localeResolver() {
+        LocaleResolver localeResolver=new SessionLocaleResolver();
+        localeResolver.setLocale("en",);
+        return new CookieLocaleResolver();
+    }*/
+/*
+    @Bean(name = "localeResolver")
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver= new CookieLocaleResolver();
+        resolver.setCookieDomain("myAppLocaleCookie");
+        // 60 minutes
+
+        resolver.setCookieMaxAge(60*60);
+        return resolver;
+    }*/
+
+
+
+
+/*    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("language");
+        return lci;
+    }*/
+
+   // @Override
+  //  public void addInterceptors(InterceptorRegistry registry) {
+  //      registry.addInterceptor(localeChangeInterceptor());
+//    }
+/*    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor).addPathPatterns("/*");
+    }*/
 }
